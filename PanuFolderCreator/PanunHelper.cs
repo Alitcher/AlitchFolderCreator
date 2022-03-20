3bcc7d0738ee76982ee8805587ba9113356874c3
alicia22nzd <97521871+AliciaInWildchain@users.noreply.github.com>
2022-03-20 19:05:51 +0700

    create helper class

---
+++ PanuFolderCreator/PanunHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanuFolderCreator
+{
+    public class PanunHelper
+    {
+        public static readonly string[] ExtensionCollection = new string[]
+{
+            ".MPG",
+            ".MP2",
+            ".MPEG",
+            ".MPE",
+            ".MPV",
+            ".OGG",
+            ".MP4",
+            ".M4P" ,
+            ".M4V" ,
+            ".AVI",
+            ".WMV",
+            ".MOV",
+            ".QT",
+            ".FLV",
+            ".SWF",
+            "AVCHD"
+};
+    }
+}
