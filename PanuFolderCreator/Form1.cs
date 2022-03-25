6082663a481d19313145b1808555cee1650c55a5
alicia22nzd <97521871+AliciaInWildchain@users.noreply.github.com>
2022-03-26 02:28:10 +0700

    finalize project

--- PanuFolderCreator/Form1.cs
+++ PanuFolderCreator/Form1.cs
@@ -7,9 +7,12 @@ namespace PanuFolderCreator
 {
     public partial class AlicianMovieRelocator : Form
     {
-        private string[] AllFiles;
-        private Dictionary<string, bool> ExtensionGenerated = new Dictionary<string, bool> { { ".mp4", false }, { ".avi", false }, { ".ktv", false }, { ".mov", false } };
 
+        private string[] allFiles;
+        private bool isAnyExtensionsSelected = false;
+
+        private Dictionary<string, bool> ExtensionGeneratedDict = new Dictionary<string, bool>();
+        private List<CheckBox> ExtensionCheckersList = new List<CheckBox>();
         public AlicianMovieRelocator()
         {
             InitializeComponent();
@@ -17,14 +20,7 @@ namespace PanuFolderCreator
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-        }
-
-
-
-    private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
+            this.folderFromFilesText.Text = "";
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -41,44 +37,103 @@ namespace PanuFolderCreator
         {
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                selectedDirectoryText.Text = folderBrowserDialog1.SelectedPath;
             }
+
+            if (selectedDirectoryText.Text == string.Empty)
+            {
+                return;
+            }
+            allFiles = Directory.GetFiles(selectedDirectoryText.Text);
+            GenerateCheckBox();
         }
 
         private void GenerateFolderButton_Click(object sender, EventArgs e)
         {
             TraverseFiles();
-        }
-
-        private void TraverseFiles()
-        {
-            if (textBox1.Text == string.Empty) 
+            if (!isAnyExtensionsSelected)
             {
-                MessageBox.Show("directory cannot be null");
-                return;
-            }
-            AllFiles = Directory.GetFiles(textBox1.Text);
-
-            label2.Text = string.Empty;
-            for (int i = 0; i < AllFiles.Length; i++)
-            {
-                CreateFolder(AllFiles[i]);
-                RelocateFiles(AllFiles[i]);
+                MessageBox.Show($"Please select atleast one extenstion to create the folder for your videos.");
             }
             MessageBox.Show("done");
         }
 
+        private void TraverseFiles()
+        {
+            if (selectedDirectoryText.Text == string.Empty)
+            {
+                MessageBox.Show("directory cannot be null");
+                return;
+            }
+
+            folderFromFilesText.Text = string.Empty;
+            for (int i = 0; i < allFiles.Length; i++)
+            {
+                CreateFolder(allFiles[i]);
+                RelocateFiles(allFiles[i]);
+            }
+
+        }
+
+        private void GenerateCheckBox()
+        {
+            int extensionPerRow = 9;
+
+            int rowCount = 0;
+            int memberInRowCount = 0;
+            int extensionCount = 0;
+            for (int i = 0; i < allFiles.Length; i++)
+            {
+                foreach (var item in PanunHelper.ExtensionCollection)
+                {
+                    string lowerExt = item.ToLower();
+                    if (ExtensionGeneratedDict.ContainsKey(lowerExt))
+                        continue;
+
+                    if (allFiles[i].EndsWith(lowerExt))
+                    {
+                        ExtensionGeneratedDict.Add(lowerExt, true);
+
+                        if (memberInRowCount > extensionPerRow)
+                        {
+                            rowCount++;
+                            memberInRowCount = 0;
+                        }
+
+                        int xPos = (int)PanunHelper.FirstCheckPos.x + memberInRowCount * PanunHelper.XOffset;
+                        int yPos = (int)PanunHelper.FirstCheckPos.y + rowCount * PanunHelper.YOffset;
+
+                        CheckBox checker = new CheckBox();
+                        checker.AutoSize = true;
+                        checker.Location = new System.Drawing.Point(xPos, yPos);
+                        checker.Name = lowerExt;
+                        checker.Size = new System.Drawing.Size(61, 21);
+                        checker.TabIndex = 7;
+                        checker.Text = lowerExt;
+                        checker.UseVisualStyleBackColor = true;
+                        checker.CheckedChanged += new EventHandler(ExtensionsCheckedChanged);
+                        this.Controls.Add(checker);
+                        memberInRowCount++;
+                        extensionCount++;
+
+                        ExtensionCheckersList.Add(checker);
+                    }
+                }
+            }
+        }
+
         private void CreateFolder(string file)
         {
-            foreach (var extensionIndex in ExtensionGenerated.Keys)
+            foreach (var extensionIndex in ExtensionGeneratedDict.Keys)
             {
-                if (file.EndsWith(extensionIndex) && ExtensionGenerated[extensionIndex] == true)
+                if (file.EndsWith(extensionIndex) && ExtensionGeneratedDict[extensionIndex] == true)
                 {
+                    isAnyExtensionsSelected = true;
                     string folderName = Path.GetFileNameWithoutExtension(file);
-                    if (!Directory.Exists(textBox1.Text + $"\\{folderName}"))
+                    if (!Directory.Exists(selectedDirectoryText.Text + $"\\{folderName}"))
                     {
-                        label2.Text += file + Environment.NewLine;
-                        Directory.CreateDirectory(textBox1.Text + $"\\{folderName}");
+                        folderFromFilesText.Text += file + Environment.NewLine;
+                        Directory.CreateDirectory(selectedDirectoryText.Text + $"\\{folderName}");
                     }
                     else
                     {
@@ -95,49 +150,25 @@ namespace PanuFolderCreator
 
         private void RelocateFiles(string source)
         {
-            string destination = $"{textBox1.Text}/{Path.GetFileNameWithoutExtension(source)}/{Path.GetFileNameWithoutExtension(source)}{Path.GetExtension(source)}";
+            string destination = $"{selectedDirectoryText.Text}/{Path.GetFileNameWithoutExtension(source)}/{Path.GetFileNameWithoutExtension(source)}{Path.GetExtension(source)}";
             if (File.Exists(destination))
             {
-                MessageBox.Show($"{destination} already exist. Thw system is skipping this file.");
+                MessageBox.Show($"{destination} already exist. The system is skipping this file.");
                 return;
             }
 
-            foreach (var extensionIndex in ExtensionGenerated.Keys)
+            foreach (var extensionIndex in ExtensionGeneratedDict.Keys)
             {
-                if (source.EndsWith(extensionIndex) && ExtensionGenerated[extensionIndex] == true)
+                if (source.EndsWith(extensionIndex) && ExtensionGeneratedDict[extensionIndex] == true)
                 {
                     File.Move(source, destination);
                 }
             }
-
         }
 
-        private void Mp4_CheckedChanged(object sender, EventArgs e)
+        private void ExtensionsCheckedChanged(object sender, EventArgs e)
         {
-            if (!ExtensionGenerated.ContainsKey(".mp4"))
-                return;
-            ExtensionGenerated[".mp4"] = mp4Check.CheckState == CheckState.Checked;
-        }
 
-        private void Avi_CheckedChanged(object sender, EventArgs e)
-        {
-            if (!ExtensionGenerated.ContainsKey(".avi"))
-                return;
-            ExtensionGenerated[".avi"] = aviCheck.CheckState == CheckState.Checked;
-        }
-
-        private void Ktv_CheckedChanged(object sender, EventArgs e)
-        {
-            if (!ExtensionGenerated.ContainsKey(".ktv"))
-                return;
-            ExtensionGenerated[".ktv"] = ktvCheck.CheckState == CheckState.Checked;
-        }
-
-        private void Mov_CheckedChanged(object sender, EventArgs e)
-        {
-            if (!ExtensionGenerated.ContainsKey(".mov"))
-                return;
-            ExtensionGenerated[".mov"] = movCheck.CheckState == CheckState.Checked;
         }
     }
 }
